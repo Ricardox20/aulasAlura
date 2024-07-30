dfa09c1f1b976a1b7280ed87b0c7975fa268b27f
Ricardox20 <sparssbh@gmail.com>
2024-07-30 13:04:54 -0300

    troca de botes concluidas(faltando pausar temporiz ador)

--- JavaScript/elementos-DOM/Fokus-projeto-base/script.js
+++ JavaScript/elementos-DOM/Fokus-projeto-base/script.js
@@ -4,17 +4,23 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const temporizador = document.querySelector('.app__card-timer')
 const comecar = document.querySelector('.app__card-primary-button-wrapper')
+const titulo = document.querySelector('.app__title')
+const tituloNegrito = document.querySelector('.app__title-strong')
+const imagemTexto = document.querySelector('.app__image')
 const segundosFoco = 1500 //60 * 25
 const segundosCurto = 300 //60 * 5
 const segundosLongo = 900 //60 * 15
 var intervalo
-var tempoSegundo
+var tempoSegundo = 1500
 temporizador.innerHTML = '25:00'
 
 focoBt.addEventListener('click', () =>{
     clearInterval(intervalo)
     temporizador.innerHTML = '25:00'
     html.setAttribute('data-contexto', 'foco')
+    tituloNegrito.innerHTML = 'mergulhe no que importa.'
+    titulo.innerHTML = 'Otimize sua produtividade, ' + tituloNegrito.outerHTML
+    imagemTexto.src='/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/foco.png'
     tempoSegundo = segundosFoco
 })
 
@@ -22,6 +28,9 @@ curtoBt.addEventListener('click', () =>{
     clearInterval(intervalo)
     temporizador.innerHTML = '05:00'
     html.setAttribute('data-contexto', 'descanso-curto')
+    tituloNegrito.innerHTML = 'Faça uma pausa curta!'
+    titulo.innerHTML = 'Que tal dar uma respirada? ' + tituloNegrito.outerHTML
+    imagemTexto.src='/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/descanso-curto.png'
     tempoSegundo = segundosCurto
 })
 
@@ -29,6 +38,9 @@ longoBt.addEventListener('click', () =>{
     clearInterval(intervalo)
     temporizador.innerHTML = '15:00'
     html.setAttribute('data-contexto', 'descanso-longo')
+    tituloNegrito.innerHTML = 'Faça uma pausa longa!'
+    titulo.innerHTML = 'Hora de voltar à superfície. ' + tituloNegrito.outerHTML
+    imagemTexto.src='/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/descanso-longo.png'
     tempoSegundo = segundosLongo
 })
 
