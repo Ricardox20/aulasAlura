d08cd4cc0446235f2709cd4a2ee3a4988cf62269
Ricardox20 <sparssbh@gmail.com>
2024-07-31 12:22:38 -0300

    projeto finalizado

--- JavaScript/elementos-DOM/Fokus-projeto-base/script.js
+++ JavaScript/elementos-DOM/Fokus-projeto-base/script.js
@@ -2,63 +2,100 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
-const temporizador = document.querySelector('.app__card-timer')
-const comecar = document.querySelector('.app__card-primary-button-wrapper')
+const botoes = document.querySelectorAll('.app__card-button')
 const titulo = document.querySelector('.app__title')
 const tituloNegrito = document.querySelector('.app__title-strong')
 const imagemTexto = document.querySelector('.app__image')
-const segundosFoco = 1500 //60 * 25
-const segundosCurto = 300 //60 * 5
-const segundosLongo = 900 //60 * 15
-var intervalo
-var tempoSegundo = 1500
-temporizador.innerHTML = '25:00'
+const musicaToggle = document.querySelector('#alternar-musica')
+const musica = new Audio('/JavaScript/elementos-DOM/Fokus-projeto-base/sons/luna-rise-part-one.mp3')
+musica.loop = true
+const comecarBt = document.querySelector('.app__card-primary-button-wrapper')
+const comecarPararBt = document.querySelector('#start-pause span')
+const play = new Audio('/JavaScript/elementos-DOM/Fokus-projeto-base/sons/play.wav')
+const pause = new Audio('/JavaScript/elementos-DOM/Fokus-projeto-base/sons/pause.mp3')
+const alerta = new Audio('/JavaScript/elementos-DOM/Fokus-projeto-base/sons/beep.mp3')
+const iconComecarParar = document.querySelector('.app__card-primary-butto-icon')
+const temporizador = document.querySelector('.app__card-timer')
+
+let tempoCorrido = 1500
+let intervaloId = null
+
+function alterarVisual(visual){
+    html.setAttribute('data-contexto', visual)
+    imagemTexto.src=`/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/${visual}.png`
+    botoes.forEach(function(visual){visual.classList.remove('active')})
+}
+
+function alterarTexto(texto1, texto2){
+    tituloNegrito.innerHTML = texto2
+    titulo.innerHTML = texto1 + tituloNegrito.outerHTML
+}
 
 focoBt.addEventListener('click', () =>{
-    clearInterval(intervalo)
-    temporizador.innerHTML = '25:00'
-    html.setAttribute('data-contexto', 'foco')
-    tituloNegrito.innerHTML = 'mergulhe no que importa.'
-    titulo.innerHTML = 'Otimize sua produtividade, ' + tituloNegrito.outerHTML
-    imagemTexto.src='/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/foco.png'
-    tempoSegundo = segundosFoco
+    alterarVisual('foco')
+    alterarTexto('Otimize sua produtividade, ','mergulhe no que importa.')
+    focoBt.classList.add('active')
+    tempoCorrido = 1500
+    mostrarTempo()
 })
 
 curtoBt.addEventListener('click', () =>{
-    clearInterval(intervalo)
-    temporizador.innerHTML = '05:00'
-    html.setAttribute('data-contexto', 'descanso-curto')
-    tituloNegrito.innerHTML = 'Faça uma pausa curta!'
-    titulo.innerHTML = 'Que tal dar uma respirada? ' + tituloNegrito.outerHTML
-    imagemTexto.src='/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/descanso-curto.png'
-    tempoSegundo = segundosCurto
+    alterarVisual('descanso-curto')
+    alterarTexto('Que tal dar uma respirada? ', 'Faça uma pausa curta!')
+    curtoBt.classList.add('active')
+    tempoCorrido = 300
+    mostrarTempo()
 })
 
 longoBt.addEventListener('click', () =>{
-    clearInterval(intervalo)
-    temporizador.innerHTML = '15:00'
-    html.setAttribute('data-contexto', 'descanso-longo')
-    tituloNegrito.innerHTML = 'Faça uma pausa longa!'
-    titulo.innerHTML = 'Hora de voltar à superfície. ' + tituloNegrito.outerHTML
-    imagemTexto.src='/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/descanso-longo.png'
-    tempoSegundo = segundosLongo
+    alterarVisual('descanso-longo')
+    alterarTexto('Hora de voltar à superfície. ', 'Faça uma pausa longa!')
+    longoBt.classList.add('active')
+    tempoCorrido = 900
+    mostrarTempo()
 })
 
-comecar.addEventListener('click', () =>{
-    if (intervalo){
-        clearInterval(intervalo)
-        intervalo = null
-    }
-    let tempo = tempoSegundo
-    let doisPontos = ':'
-    intervalo = setInterval(() => {
-        if(tempo > 0){
-            tempo--
-            let minutos = Math.floor(tempo/60)%60 > 9 ? Math.floor(tempo/60)%60 : '0'+Math.floor(tempo/60)%60
-            let segundos = Math.floor(tempo)%60 > 9 ? Math.floor(tempo)%60 : '0'+Math.floor(tempo)%60
-            temporizador.innerHTML = minutos+doisPontos+segundos
-        } else {
-            temporizador.innerHTML = '00'+doisPontos+'00'
-        }
-    }, 1000);
+musicaToggle.addEventListener('click', ()=>{
+    musica.paused? musica.play() : musica.pause()
 })
+
+const contagemRegressiva = () => {
+    if(tempoCorrido <= 0){
+        alerta.play()
+        alert('Tempo encerrado!')
+        zerar()
+        return
+    }
+    tempoCorrido --
+    mostrarTempo()
+}
+
+comecarBt.addEventListener('click', startStop)
+
+function startStop(){
+    if(intervaloId){
+        pause.play()
+        zerar()
+        comecarPararBt.textContent = "Começar"
+        iconComecarParar.src = `/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/play_arrow.png`
+        return
+    } else {
+        play.play()
+        comecarPararBt.textContent = "Parar"
+        iconComecarParar.src = `/JavaScript/elementos-DOM/Fokus-projeto-base/imagens/pause.png`
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    intervaloId = null
+}
+
+function mostrarTempo(){
+    const tempo = new Date(tempoCorrido * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-BR', {minute: '2-digit', second: '2-digit'})
+    temporizador.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo()
